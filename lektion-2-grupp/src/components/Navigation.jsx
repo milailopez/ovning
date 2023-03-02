2fbc7a1380853e476d5319c59377157b9460c739
Milai Lopez <milai.lopez@icloud.com>
2023-03-02 13:22:17 +0100

    milai css

--- lektion-2-grupp/src/components/Navigation.jsx
+++ lektion-2-grupp/src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navigation = () => {
   return (
     <nav className='nav'>
-      <div>
+      <div className='hamburger'>
         <span></span><span></span><span></span>
       </div>
     </nav>
